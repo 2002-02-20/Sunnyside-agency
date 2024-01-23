6bf9c9177f3bb5ca7422f01b2d50b7fad1dcabfd
Derek <adhoc607@gmail.com>
2024-01-23 17:24:37 -0500

    part 2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Services } from './components/Services'
+
+function App() {
+
+  return (
+    <main className='max-w-[1440px] mx-auto relative'>
+
+    <Services/>
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      
+      screens: {
+        tablet: "700px",
+        desktop: "1440px",
+      }
+    },
+  },
+  plugins: [],
+};
