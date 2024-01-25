a1c2927b40cbf1131ed48974c503a3917c73559a
Derek <adhoc607@gmail.com>
2024-01-25 10:06:23 -0500

    components and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import { Services } from './components/Services'
+import { Main } from "./components/Main";
+import { Header } from "./components/Header";
+import { About } from "./components/About";
+import { TestimonialContainer } from "./components/TestimonialContainer";
+import { Gallery } from "./components/Gallery";
+import { Footer } from "./components/Footer";
 
 function App() {
-
   return (
-    <main className='max-w-[1440px] mx-auto relative'>
-
-    <Services/>
+    <main className="max-w-full mx-auto relative">
+      <Header />
+      <Main />
+      <About />
+      <TestimonialContainer />
+      <Gallery />
+      <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
